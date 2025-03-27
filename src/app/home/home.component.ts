16051eab345badf0c8cc096ac1385dfc4dfce9b3
DANIEL\moren <daniel moreno>
2025-03-27 14:02:00 -0500

    vista -cambios en el componente-home importaciones inecesarias

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
+import { Component, } from '@angular/core';
 import { MenuSuperiorComponent } from '../components/menu-superior/menu-superior.component';
 import { SeccionVideoComponent } from '../components/seccion-video/seccion-video.component';
 import { SeccionEstanciasComponent } from '../components/seccion-estancias/seccion-estancias.component';
@@ -15,7 +14,7 @@ import { SeccionFooterComponent } from '../components/seccion-footer/seccion-foo
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule,CommonModule,MenuSuperiorComponent,SeccionVideoComponent,SeccionEstanciasComponent,
+  imports: [MenuSuperiorComponent,SeccionVideoComponent,SeccionEstanciasComponent,
     SeccionCaratulasComponent,SeccionExploraComponent,SeccionColleccionComponent,SeccionTestimoniosComponent,
     SeccionPropietariosComponent,SeccionHeroComponent,SeccionFooterComponent
   ],
@@ -23,40 +22,5 @@ import { SeccionFooterComponent } from '../components/seccion-footer/seccion-foo
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  selectedCurrency: string = 'ES-€'; 
-  menuActive: boolean = false;
-  numero:number=1
   
-  disminuirNumero(){
-    if(this.numero>1){
-    this.numero=this.numero-1
-  }
-  }
-
-  aumentarNumero(){
-    if(this.numero<10){
-    this.numero=this.numero+1
-  }
-  }
-
-  // Función que cambia el estado del menú
-  toggleMenu() {
-    this.menuActive = !this.menuActive;
-  }
-
-   @ViewChild('carousel') carousel!: ElementRef;
-
-  scrollLeft() {
-    this.carousel.nativeElement.scrollBy({
-      left: -600,
-      behavior: 'smooth'
-    });
-  }
-
-  scrollRight() {
-    this.carousel.nativeElement.scrollBy({
-      left: 6000, // Aumenté de 3000 a 6000 (el doble de desplazamiento)
-      behavior: 'smooth'
-    });
-  }
 }
